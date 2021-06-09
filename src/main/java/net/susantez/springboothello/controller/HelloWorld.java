0153c292d49a5a03546b183e356f4f7429f6edc4
susantez <serkan.susantez@teamdefinex.com>
2021-06-09 11:54:44 +0300

    Initial Commit **hello-world**

---
+++ src/main/java/net/susantez/springboothello/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.susantez.springboothello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "world";
+    }
+}
